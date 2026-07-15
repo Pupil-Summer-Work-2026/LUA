0568b6984a09ae7afb9cad5e13e911dae5cf3950
DavidsAsmanis <davids.asmanis@accenture.com>
2026-07-15 11:47:10 +0300

    Slight improvments

--- src/views/about-us.jsx
+++ src/views/about-us.jsx
@@ -42,8 +42,13 @@ function AboutUs() {
           <div>
             <span className="lua-eyebrow">PAR ASOCIĀCIJU</span>
             <h1>KAS IR <em>LUA</em>?</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+            <p>Biedrības mērķis ir Latvijas Republikas ugunsdzēsības un ugunsdrošības servisa uzņēmumu un darbinieku apvienošana kopīgam radošam darbam un to interešu aizsardzība.</p>
+            <br/>
+            <p>Pēc savas darbības un organizatoriskās uzbūves “Latvijas Ugunsdrošības asociācija” ir brīvprātīga, profesionāla biedrība, kuras pamatmērķis ir sekmēt tās biedru savstarpējo sadarbību un profesionālo izaugsmi, aizstāvēt savu biedru saimnieciskās, ekonomiskās un tiesiskās intereses, kā arī aktīva darbība Latvijas ugunsdzēsības un ugunsdrošības pakalpojumu sniegšanas tirgū un savu biedru pārstāvniecība pašvaldību un valsts institūcijās. “Latvijas Ugunsdrošības asociācija” ir vienīgā šāda profila sabiedriska organizācija Latvijā.</p>
+            <br/>
+            <p>Kā svarīgāko uzdevumu savai darbībai Biedrība izvirza iedzīvotāju ugunsdrošības nodrošināšanu valstī un uzskata, ka to var panākt ar vienotu darbību likumdošanas un normatīvās bāzes pilnveidošanā, kvalitatīvu ugunsdrošības, pakalpojumu sniegšanu, profesionālu speciālistu sagatavošanu un efektīvu sabiedrības informēšanu jautājumos par ugunsdrošību. Īpašā uzmanība, pēc Biedrība biedru domām, šo jautājumu risināšanā un aktualizēšanā sabiedrībā ir jāveltī sadarbībai ar masu informācijas līdzekļiem.</p>
+            <br/>
+            <p>“Latvijas Ugunsdrošības asociācija” dibināta 2002. gadā un uz pašreizējo brīdi apvieno ap 30 Latvijas un starptautiskos vadošos ugunsdzēsības un ugunsdrošības servisa uzņēmumus. Biedrība sadarbojas ar citām sabiedriskām organizācijām. No 2008. gada “Latvijas Ugunsdrošības asociācija” ir Iekšlietu Ministrijas sabiedrības Konsultatīvās drošības padomes biedrs un vada darba grupu “Ugunsdrošība un civilā aizsardzība”. “Latvijas Ugunsdrošības asociācija” ir “Latvijas Darba Devēju Konfederācijas” biedrs.</p>
           </div>
         </section>
         <section className="about-page__mission">
@@ -51,11 +56,14 @@ function AboutUs() {
             <div>
               <span className="lua-eyebrow">PAR NODARBOŠANOS</span>
               <h2>MŪSU LOMA <em>UGUNSDROŠĪBĀ</em></h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p>Asociācijas ikdienas un radošais darbs tiek plānots un norisinās pa vairākām darba grupām:</p>
+              <br/>
               <ul>
-                <li>Lorem ipsum dolor sit amet</li>
-                <li>Lorem ipsum dolor sit amet</li>
-                <li>Lorem ipsum dolor sit amet</li>
+                <li>Ugunsdzēsības aparāti un to apkalpošana.</li>
+                <li>Ugunsdzēsības automātika un signalizācijas sistēmas, to projektēšana, uzstādīšana un apkalpošana.</li>
+                <li>Būvtehniskā ugunsaizsardzība.</li>
+                <li>Dūmvadi, ventilācija. To uzstādīšana un apkalpošana.</li>
+                <li>Ugunsdrošības speciālistu apmācība.</li>
               </ul>
             </div>
             <img src="/Images/lit-match.jpg" alt="Aizdegta sērkociņa liesma" />
