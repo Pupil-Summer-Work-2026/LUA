29ef6c4924900e34940d0b42c58b86798357c843
1MrArk <intars.boldans@icloud.com>
2026-07-15 14:30:05 +0300

    update run.bat & optimize font loading

--- run.bat
+++ run.bat
@@ -2,8 +2,15 @@
 echo This is a batch file to set up a development environment. Do NOT use this in production.
 echo Please ensure that you have Python 3.12 or higher installed and added to your PATH.
 if not exist "venv\Scripts\python.exe" (
-	python -m venv venv
-	if errorlevel 1 exit /b 1
+    python -m venv venv
+    if errorlevel 1 exit /b 1
+    venv\Scripts\python.exe -m pip install --upgrade pip
+    venv\Scripts\python.exe -m pip install -r requirements.txt
 )
-start "" cmd /k "venv\Scripts\python.exe -m pip install --upgrade pip && venv\Scripts\python.exe -m pip install -r requirements.txt && venv\Scripts\python.exe manage.py migrate && venv\Scripts\python.exe manage.py runserver"
-start "" cmd /k "npm.cmd install && npm.cmd run dev"
+
+if not exist "node_modules" (
+    npm.cmd install
+)
+
+start "" cmd /k "venv\Scripts\python.exe manage.py migrate && venv\Scripts\python.exe manage.py runserver"
+start "" cmd /k "npm.cmd run dev"
